4bd94fc6358b36379a13bf342c4debe1b2aeb706
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2022-12-11 19:09:29 +0300

    Day11 part2

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,7 +1,8 @@
 fun main() {
     val input = readInput("Day11_test")
     val monkeys = mutableListOf<Monkey>()
-    input.filter { it.isNotEmpty() }.chunked(6).map {
+    val divideOnList = mutableListOf<Int>()
+    input.filter { it.isNotEmpty() }.chunked(6).forEach {
         val items = it[1].substringAfterLast("Starting items: ").split(", ").map(String::toInt)
         val operation = it[2].substringAfterLast("Operation: new = ").split(" ")
         val divisibleBy = it[3].substringAfterLast("Test: divisible by ").toInt()
@@ -9,8 +10,7 @@ fun main() {
         val ifFalseTo = it[5].substringAfterLast("If false: throw to monkey ").toInt()
         monkeys.add(
             Monkey(
-                items = items.toMutableList(),
-                divideOn = divisibleBy,
+                items = items.map(Monkey::Item).toMutableList(),
                 ifTrueTo = ifTrueTo,
                 ifFalseTo = ifFalseTo,
                 operation = Monkey.Operation(
@@ -19,41 +19,65 @@ fun main() {
                 )
             )
         )
+        divideOnList.add(divisibleBy)
     }
-    for (i in 0 until 20) {
-        monkeys.forEach { monkey ->
-            val iterator = monkey.items.iterator()
+
+    monkeys.forEach {
+        it.items.forEach { item ->
+            val list = mutableListOf<Int>()
+            divideOnList.forEach { divider ->
+                list.add(item.value % divider)
+            }
+            item.divisionResult.addAll(list)
+        }
+    }
+
+    for (i in 0 until 10_000) {
+        monkeys.forEachIndexed { monkeyIndex, monkey ->
             monkey.intersections += monkey.items.size
+            val iterator = monkey.items.iterator()
             while (iterator.hasNext()) {
                 val it = iterator.next()
-                val item = (if (monkey.operation.action == "*") {
-                    it * (monkey.operation.value ?: it)
+                if (monkey.operation.action == "*") {
+                    it.divisionResult = it.divisionResult.map { div ->
+                        div * (monkey.operation.value ?: div)
+                    }.toMutableList()
                 } else {
-                    it + (monkey.operation.value ?: it)
-                }) / 3
-                if (item % monkey.divideOn == 0) {
-                    monkeys[monkey.ifTrueTo].items.add(item)
+                    it.divisionResult = it.divisionResult.map { div ->
+                        div + (monkey.operation.value ?: div)
+                    }.toMutableList()
+                }
+                for (ind in 0 until it.divisionResult.size) {
+                    it.divisionResult[ind] %= divideOnList[ind]
+                }
+                if (it.divisionResult[monkeyIndex] == 0) {
+                    monkeys[monkey.ifTrueTo].items.add(it)
                 } else {
-                    monkeys[monkey.ifFalseTo].items.add(item)
+                    monkeys[monkey.ifFalseTo].items.add(it)
                 }
                 iterator.remove()
             }
         }
     }
 
-    val result = monkeys.map { it.intersections }.sortedDescending().take(2).reduce { acc, i -> acc * i }
+    val result = monkeys.map { it.intersections.toLong() }.sortedDescending().take(2).reduce { acc, i -> acc * i }
 
     println(result)
 }
 
 class Monkey(
-    val items: MutableList<Int>,
-    val divideOn: Int,
+    val items: MutableList<Item>,
     val ifTrueTo: Int,
     val ifFalseTo: Int,
     val operation: Operation,
-    var intersections: Int = 0
+    var intersections: Int = 0,
 ) {
+
+    class Item(
+        val value: Int,
+        var divisionResult: MutableList<Int> = mutableListOf()
+    )
+
     class Operation(
         val action: String,
         val value: Int?
